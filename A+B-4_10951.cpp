1030db79531cacc639a7fb2513611ea4a6553f92
GyuHyeonWkdWkdMan <64968404+GyuHyeonWkdWkdMan@users.noreply.github.com>
2021-01-07 01:40:05 +0900

    Add files via upload

---
+++ A+B-4_10951.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+
+int main()
+{
+    int a;
+    int b;
+    //cin.tie(NULL);
+    //ios_base::sync_with_stdio(false);
+    while(cin>>a>>b)
+    {
+        int c = a+b;
+
+        cout<<c<<"\n";
+    }
+    return 0;
+}

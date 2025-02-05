0e58e85d96a229213b02d167647209acc6646549
GyuHyeonWkdWkdMan <nero0213@naver.com>
2025-02-05 13:45:54 +0900

    java 1037

---
+++ java/b1037.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class b1037 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int N = sc.nextInt();
+        
+        int[] a = new int[N];
+        for(int i = 0; i < N; i++){
+            a[i] = sc.nextInt();
+        }
+        int max = 0;
+        int min = 1000001;
+        for (int i = 0; i < N; i++) {
+            if(a[i] > max){
+                max = a[i];
+            }
+            if(a[i] < min){
+                min = a[i];
+            }
+        }
+        int result = max * min ;
+        System.out.println(result);
+        sc.close();
+    }
+}
